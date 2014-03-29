7315c34010ccf8de635ccc6bee5f9812a48b8f43
jameshoare <jhoare0@gmail.com>
2014-03-29 08:33:34 +0000

    gitignore

--- src/main/java/com/netaporter/booty/api/BootyController.java
+++ src/main/java/com/netaporter/booty/api/BootyController.java
@@ -4,11 +4,13 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.*;
+
 /**
  * Created by jameshoare on 28/03/2014.
  */
 @RestController
-@RequestMapping(value = "/booty",produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/booty",produces = APPLICATION_JSON_VALUE)
 public class BootyController {
 
     @RequestMapping
