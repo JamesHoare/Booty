f233ceebaa14694ffc20641133f6b819a7b73f84
jameshoare <jhoare0@gmail.com>
2014-04-05 16:27:49 +0100

    tests with GS Collections

--- src/test/java/CustomerPredicateTest.java
+++ src/test/java/CustomerPredicateTest.java
@@ -1,8 +1,10 @@
 import com.gs.collections.api.list.ImmutableList;
 import com.gs.collections.api.list.MutableList;
 import com.gs.collections.impl.list.mutable.FastList;
+import com.gs.collections.impl.test.Verify;
 import com.netaporter.booty.domain.Customer;
 import org.junit.Test;
+import org.junit.Assert;
 
 import java.util.stream.*;
 import java.util.*;
@@ -32,17 +34,21 @@ public class CustomerPredicateTest {
                 customer.parallelStream().filter((Customer c) -> c.getFirstName().equals("James")).collect(toList());
 
         //List<Customer> filteredCustomers = filterCustomers(customer, Customer::isJames);
-        System.out.println(filteredCustomers);
+        //System.out.println(filteredCustomers);
 
 
+    }
+
+    @Test
+    public void gsCollectionsTest() {
         /**
          * gs collection and lambdas
          */
         MutableList<Integer> expectedNames = FastList.newListWith(50,100,40);
-        List<Integer> greaterThanFifty =
-                expectedNames.select(each -> each > 50);
-        System.out.println(greaterThanFifty);
 
+        Verify.assertContainsAll(expectedNames.select(each -> each > 50),100);
+
+        Verify.assertContainsAll(expectedNames.reject(each -> each > 50),50,40);
 
 
     }
